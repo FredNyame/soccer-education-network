9fff9fd2d4123db1781a3fcbe89edd59db312781
[frednyame] <frednyame63@gmail.com>
2020-06-07 11:17:12 -0500

    Blog styling completed

--- assests/js/components/nav.js
+++ assests/js/components/nav.js
@@ -7,19 +7,22 @@
     //Get the element
     const menuBtn = document.querySelector('.mobile-container');
     const dateElm = document.querySelector('#date');
+    let mainNav = document.querySelector('.main_nav');
   
     //Use Js to change the year for the footer Copyright
     dateElm.innerHTML = new Date().getFullYear();
   
     //li items
     const menuItems = document.querySelectorAll('.menu-item');
+    
     //Add event to click
     menuBtn.addEventListener('click', showMenu);
   
     //function to do the showing of the elements
     function showMenu(){
       menuBtn.classList.toggle('change');
-      document.querySelector('.main_nav').classList.toggle('right-show');
+      mainNav.classList.toggle('right-show');
+      mainNav.classList.toggle('mobile_nav');
       menuItems.forEach(item => item.classList.toggle('right-show'));
     }
   });
--- single.php
+++ single.php
@@ -29,9 +29,19 @@ get_header();
 										<?= the_time('F jS, Y');?>
 									</div>
 									<div class="share-link">
-										Share
-										<ul class="social-icons">
-											<li>Facebook</li>
+										<p>Share</p>
+
+										<ul class="social-logos">
+											<li>
+												<a href="https://www.facebook.com/sharer/sharer.php?u=<?= get_permalink();?>" target="_blank">
+													<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 21.779 42.239"><defs><style>.a{fill:#fff;}</style></defs><path class="a" d="M6.328,42.239V23.347H0V15.839H6.328V9.924C6.328,3.5,10.254,0,15.988,0a53.123,53.123,0,0,1,5.791.3V7.012H17.8c-3.118,0-3.721,1.485-3.721,3.655v5.173h7.037l-.965,7.507H14.082V42.239"/></svg>
+												</a>
+											</li>
+											<li>
+												<a href="https://twitter.com/intent/tweet?text=%22<?php echo the_title(); ?>%22%20<?php echo get_permalink(); ?>" target="_blank">
+													<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50.574 41.075"><defs><style>.a{fill:#fff;}</style></defs><g transform="translate(-454.949 216)"><path class="a" d="M45.375,58.319c.032.449.032.9.032,1.348,0,13.7-10.429,29.491-29.491,29.491A29.291,29.291,0,0,1,0,84.5a21.443,21.443,0,0,0,2.5.128A20.758,20.758,0,0,0,15.371,80.2,10.383,10.383,0,0,1,5.68,73.016a13.071,13.071,0,0,0,1.958.16,10.962,10.962,0,0,0,2.728-.353A10.366,10.366,0,0,1,2.054,62.651v-.128a10.439,10.439,0,0,0,4.685,1.316A10.381,10.381,0,0,1,3.53,49.975,29.462,29.462,0,0,0,24.9,60.822a11.7,11.7,0,0,1-.257-2.375,10.375,10.375,0,0,1,17.938-7.092,20.407,20.407,0,0,0,6.578-2.5,10.337,10.337,0,0,1-4.557,5.712,20.779,20.779,0,0,0,5.969-1.6A22.281,22.281,0,0,1,45.375,58.319Z" transform="translate(454.949 -264.082)"/></g></svg>
+												</a>
+											</li>
 										</ul>
 									</div>
 								</div>
